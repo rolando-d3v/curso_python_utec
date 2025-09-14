eaf7ce19a5c7c43be7c86eddabd3b31c8ebbce07
rolando-d3v <landon3388@gmail.com>
2025-09-13 21:10:42 -0500

    Initial commit

--- 01/condicionales.py
+++ 01/condicionales.py
@@ -1,37 +1,37 @@
-monto = int(input("Ingrese su monto: "))
+# monto = int(input("Ingrese su monto: "))
 
-if monto >= 1000:
-    print("Es mayor a 1000")
-else:
-    print("Es menor a 1000")
+# if monto >= 1000:
+#     print("Es mayor a 1000")
+# else:
+#     print("Es menor a 1000")
     
     
 # Ejercicio 1: Clasificación de riesgo Dado el ingreso mensual y el nivel de deuda,
 # imprimir: "Riesgo Bajo" si deuda < 30% del ingreso. "Riesgo Medio" 
 # si deuda entre 30% y 50%. "Riesgo Alto" si deuda > 50%.
 
-ingreso = int(input("Registre su ingreso: "))
-deuda = int(input("Registre su deuda: "))
+# ingreso = int(input("Registre su ingreso: "))
+# deuda = int(input("Registre su deuda: "))
 
-if deuda < 0.3 * ingreso:
-    print("Riesgo Bajo")
-elif deuda < 0.5 * ingreso:
-    print("Riesgo Medio")
-else:
-    print("Riesgo Alto")
+# if deuda < 0.3 * ingreso:
+#     print("Riesgo Bajo")
+# elif deuda < 0.5 * ingreso:
+#     print("Riesgo Medio")
+# else:
+#     print("Riesgo Alto")
  
 
 # Ejercicio 2: Simulación de aprobación Preguntar ingreso, score y deudas.
 # Aprobar solo si: ingreso ≥ 5000 score ≥ 700 deuda ≤ 40% ingreso.
 
-ingreso = int(input("Registre su ingreso: "))
-score = int(input("Registre su score: "))
-deuda = int(input("Registre su deuda: "))
+# ingreso = int(input("Registre su ingreso: "))
+# score = int(input("Registre su score: "))
+# deuda = int(input("Registre su deuda: "))
 
-if ingreso >= 5000 and score >= 700 and deuda <= 0.4 * ingreso:
-    print("Aprobado")
-else:
-    print("Rechazado")
+# if ingreso >= 5000 and score >= 700 and deuda <= 0.4 * ingreso:
+#     print("Aprobado")
+# else:
+#     print("Rechazado")
  
 
 # Ejercicio 3: Rango de montos de crédito Según ingreso,
@@ -39,16 +39,16 @@ else:
 # hasta 50,000 < 7000: hasta 20,000
 
 
-ingreso = int(input("Registre su ingreso: "))
+# ingreso = int(input("Registre su ingreso: "))
 
-if ingreso >= 10000:
-  print("Hasta 80,000")
+# if ingreso >= 10000:
+#   print("Hasta 80,000")
 
-elif ingreso >= 7000:
-  print("Hasta 50,000")
+# elif ingreso >= 7000:
+#   print("Hasta 50,000")
 
-else:
-  print("Hasta 20,000")
+# else:
+#   print("Hasta 20,000")
 
  
  
